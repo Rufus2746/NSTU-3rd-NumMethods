23fa232886ae7142970389187f7e3e2984ae5091
Nikita Palii <rufus2746@gmail.com>
2023-10-05 03:56:09 +0700

    Input was change, still have some questions

--- Pt1/SOLE.cpp
+++ Pt1/SOLE.cpp
@@ -1,26 +1,24 @@
 #include <iostream>
-#include <vector>
-#include <string>
 #include <iomanip>
 #include <fstream>
 #include <cstdio>
 
-#include <cstdlib>
-#include <ctime>
+#include <cstdlib>                                  //Delete at the end
+#include <ctime>                                    //Delete at the end
 
 
 using namespace std;
 
 void read_dimensions(int *n, int *m)
 {
-    ifstream dimention("dimention.txt");
-    dimention >> *n;
-    dimention >> *m;
-    dimention.close();
+    ifstream dimension("dimention.txt");
+    dimension >> *n;
+    dimension >> *m;
+    dimension.close();
 }
 
 void create_random_matryx(int n, int m){            //Delete at the end
-    ofstream alFile("al.txt");
+    ofstream alFile("al.txt");                      //A nahui ya eto delal
     int x;
     for (int i = 0; i < n; i++)
     {
@@ -36,7 +34,6 @@ void create_random_matryx(int n, int m){            //Delete at the end
 }
 
 void read_matrices(int n, int m, double **al, double *d, double **au, double *vec_b, double **L, double **U){
-    printf("Matrix al:\n");
     ifstream alFile("al.txt");
     for (int i = 0; i < n; i++)
     {
@@ -44,24 +41,48 @@ void read_matrices(int n, int m, double **al, double *d, double **au, double *ve
         for (int j = 0; j < m; j++)
         {
             alFile >> al[i][j];
-            printf("%10.2f ", al[i][j]);
         }
-        printf("\n");
     }
     alFile.close();
+
+    ifstream auFile("au.txt");
+    for (int i = 0; i < n; i++)
+    {
+        au[i] = new double[m];
+        for (int j = 0; j < m; j++)
+        {
+            auFile >> au[i][j];
+        }
+    }
+    auFile.close();
+
+    ifstream vecFile("vec.txt");
+    for (int i = 0; i < n; i++)
+        vecFile >> vec_b[i];
+    vecFile.close();
+
+    for (int i = 0; i < n; i++)
+    {
+        L[i] = new double[n];
+        U[i] = new double[n];
+        for (int j = 0; j < n; j++)
+        {
+            L[i][j] = 0.0;
+            U[i][j] = 0.0;
+        }
+    }
 }
 
 int main()
 {
     int n, m;
+    read_dimensions(&n, &m);
+
     double **al = new double *[n];
     double *d = new double[n];
     double **au = new double *[n];
     double *vec_b = new double[n];
     double **L = new double *[n];
     double **U = new double *[n];
-
-    read_dimensions(&n, &m);
-    create_random_matryx(n, m);                     //Delete at the end
     read_matrices(n, m, al, d, au, vec_b, L, U);
 }
